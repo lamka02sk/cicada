11ef1e67df2d32ba5d225c9fee18c275ad4c1319
lamka02sk <y0FCiFZU!j#R2718yLOa9^8*2^a!Fl&QdVYBihfQ>
2021-12-27 13:37:56 +0100

    CORS support

--- src/core/src/lib.rs
+++ src/core/src/lib.rs
@@ -4,7 +4,8 @@ mod routes;
 extern crate log;
 extern crate simplelog;
 
-use actix_web::{App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{App, http, HttpServer};
 use actix_web::web::Data;
 use cicada_common::{Cicada, FileManager, SystemConfiguration, TextFile};
 use tera::Tera;
@@ -24,36 +25,22 @@ pub async fn start() -> std::io::Result<()> {
     let configuration = configuration.unwrap();
     let system_config: &SystemConfiguration = configuration.get("system").unwrap().as_any().downcast_ref().unwrap();
 
-    // Logging
-    match TextFile::new(&system_config.logs.file).get_writer(true) {
-        Ok(file) => {
-            match CombinedLogger::init(vec![
-                TermLogger::new(system_config.logs.level.as_level(), Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
-                WriteLogger::new(system_config.logs.level.as_level(), Config::default(), file),
-            ]) {
-                Err(error) => panic!("Logging system could not be configured: {}", error),
-                _ => {}
-            };
-        },
-        Err(error) => panic!("Logging system could not be configured: {}", error)
-    };
+    initialize_logging(&system_config);
 
     info!("{} is starting...", system_config.name);
     info!("Initializing HTML templating engine...");
 
     // Tera templates
-    let tera = match Tera::new("templates/**/*.j2") {
-        Ok(tera) => tera,
-        Err(error) => {
-            panic!("Could not initialize HTML templates: {}", error);
-        }
-    };
+    let tera = initialize_templating();
 
     info!("Preparing actix-web server...");
+
+    let cors = system_config.cors.clone();
 
     // Server startup
     let mut server = HttpServer::new(move || {
         App::new()
+            .wrap(get_cors_middleware(cors.clone()))
             .app_data(Data::new(Cicada::new()))
             .app_data(Data::new(tera.clone()))
             .configure(routes::configure)
@@ -70,5 +57,53 @@ pub async fn start() -> std::io::Result<()> {
     }
 
     server.run().await
+
+}
+
+fn initialize_logging(system_config: &SystemConfiguration) {
+    match TextFile::new(&system_config.logs.file).get_writer(true) {
+        Ok(file) => {
+            match CombinedLogger::init(vec![
+                TermLogger::new(system_config.logs.level.as_level(), Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+                WriteLogger::new(system_config.logs.level.as_level(), Config::default(), file),
+            ]) {
+                Err(error) => panic!("Logging system could not be configured: {}", error),
+                _ => {}
+            };
+        },
+        Err(error) => panic!("Logging system could not be configured: {}", error)
+    };
+}
+
+fn initialize_templating() -> Tera {
+    match Tera::new("templates/**/*.j2") {
+        Ok(tera) => tera,
+        Err(error) => {
+            panic!("Could not initialize HTML templates: {}", error);
+        }
+    }
+}
+
+fn get_cors_middleware(config: Option<Vec<String>>) -> Cors {
+
+    let config = match config {
+        Some(config) => match config.len() {
+            0 => return Cors::permissive(),
+            _ => config
+        },
+        None => return Cors::permissive()
+    };
+
+    let mut cors = Cors::default()
+        .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+        .allowed_header(http::header::ACCEPT)
+        .allowed_header(http::header::CONTENT_TYPE)
+        .max_age(3600);
+
+    for domain in config {
+        cors = cors.allowed_origin(&domain);
+    }
+
+    cors
 
 }
